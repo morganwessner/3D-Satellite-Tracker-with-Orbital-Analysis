451ef7fca9ffbce3344dbf43e591bb701ecbf294
morganwessner <mgw42@pitt.edu>
2026-01-20 23:23:49 -0500

    add middleware & frontend template

--- app.py
+++ app.py
@@ -2,8 +2,17 @@
 from fastapi import FastAPI
 from datetime import datetime, timezone
 from scripts.propagate_tle import SatellitePropagator
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    alloworigins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 #initialize the satellite propagator with TLE data
 propagator = SatellitePropagator("scripts/data/tle_parsed/tle_data_parsed.json")
@@ -24,9 +33,25 @@ def satellite_state(norad_id: str):
     now = datetime.now(timezone.utc)
     position, velocity = propagator.propagate(norad_id, now)
     
+    position = [float(x) for x in position]
+    velocity = [float(x) for x in velocity]
+    
     return {
         "norad_id": norad_id,
         "time": now.isoformat(),
         "position": position,
         "velocity": velocity
     }
+    
+#enpoint to get trajectory over time for a satellite
+@app.get("/trajectory/{norad_id}")
+def trajectory(norad_id: str, duration_minutes: int = 60, step_seconds: int = 10):
+    start_time = datetime.now(timezone.utc)
+    trajectory_data = propagator.propagate_over_time(norad_id, start_time, duration_minutes, step_seconds)
+    
+    #convert positions and velocities to floats for JSON
+    for point in trajectory_data:
+        point["position"] = [float(x) for x in point["position"]]
+        point["velocity"] = [float(x) for x in point["velocity"]]
+        point["time"] = point["time"].isoformat()
+    return trajectory_data
